1f52b2e3ed8a1827ba5f081ac32d492418200702
Mark <marcosromeromtz@gmail.com>
2018-11-23 03:51:09 -0600

    second commit

---
+++ precache-manifest.f71bbf18d53913e3f96bb43d6125f0c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4577ab21deba8e5def5",
+    "url": "/static/js/main.c4577ab2.chunk.js"
+  },
+  {
+    "revision": "5e599bcf4900aaf4206a",
+    "url": "/static/js/1.5e599bcf.chunk.js"
+  },
+  {
+    "revision": "c4577ab21deba8e5def5",
+    "url": "/static/css/main.1df51fe2.chunk.css"
+  },
+  {
+    "revision": "9ef744f9616ea67f92f395b984688d0f",
+    "url": "/index.html"
+  }
+];
